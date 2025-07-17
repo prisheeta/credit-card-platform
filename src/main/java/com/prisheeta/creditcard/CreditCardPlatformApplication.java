d600bceb86e14680a67f28f8e4516c0287435f93
Prisheeta Jain <prisheetajain@Prisheetas-MacBook-Pro.local>
2025-07-18 00:51:11 +0530

    chore: initial Springboot setup with H2, Swagger and profiles

---
+++ src/main/java/com/prisheeta/creditcard/CreditCardPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.prisheeta.creditcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prisheeta/creditcard/CreditCardPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prisheeta.creditcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
